988ab2c61ee889b68045f9602318d3fbba3caa9d
PvlMamaev <95972919+PvlMamaev@users.noreply.github.com>
2022-04-09 20:24:01 +0300

    day-6 first step

---
+++ app/src/main/java/com/example/kotovod/AboutActivity.kt
@@ -0,0 +1,14 @@
+package com.example.kotovod
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.kotovod.R
+
+class AboutActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+
+}
--- app/src/main/java/com/example/kotovod/MainActivity.kt
+++ app/src/main/java/com/example/kotovod/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.kotovod
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity, AboutActivity::class.java)
+            startActivity(intent)
+        // Для запуска нового экрана необходимо создать экземпляр класса Intent
+        // и указать в первом параметре текущий класс, а во втором - класс для перехода, у нас это com.example.kotovod.AboutActivity
+        }
+
+        val button_second = findViewById<Button>(R.id.button_second)
+        button_second.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

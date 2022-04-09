b943fd33ec749c9d426d01284550bf51e492827d
PvlMamaev <95972919+PvlMamaev@users.noreply.github.com>
2022-04-09 16:07:23 +0300

    add: day4
    TrafficLight

--- app/src/main/java/com/example/kotovod/MainActivity.kt
+++ app/src/main/java/com/example/kotovod/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.kotovod
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val redButton = findViewById<Button>(R.id.button_red)
+        val yellowButton = findViewById<Button>(R.id.button_yellow)
+        val greenButton = findViewById<Button>(R.id.button_green)
+        val textView = findViewById<TextView>(R.id.textView)
+        val rootLayout = findViewById<ConstraintLayout>(R.id.root_layout)
+
+        redButton.setOnClickListener {
+            textView.text = "Красный"
+            rootLayout.setBackgroundColor(resources.getColor(R.color.redColor, null))
+        }
+
+        yellowButton.setOnClickListener {
+            textView.setText(R.string.yellow)
+            rootLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.yellowColor))
+        }
+
+        greenButton.setOnClickListener {
+            textView.text = resources.getText(R.string.green)
+            rootLayout.setBackgroundColor(Color.GREEN)
+        }
+
     }
 }

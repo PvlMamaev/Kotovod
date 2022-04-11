73c0988a41721f7b2e38a491b13ddedc189c9466
PvlMamaev <95972919+PvlMamaev@users.noreply.github.com>
2022-04-11 18:39:21 +0300

    add: sherlock finish
    finish day 6

--- app/src/main/java/com/example/kotovod/MainActivity.kt
+++ app/src/main/java/com/example/kotovod/MainActivity.kt
@@ -5,11 +5,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : Activity() { // но если там будет стоять AppCompatActivity() то 23 строка перестанет работать
 
     companion object{
         const val REQUEST_CHOOSE_THIEF = 0
+        const val THIEF = "com.example.kotovod.SecondActivity"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +24,20 @@ class MainActivity : Activity() { // но если там будет стоят�
             val questionIntent = Intent(this@MainActivity, SecondActivity::class.java)
             startActivityForResult(questionIntent, REQUEST_CHOOSE_THIEF) // работает только если наследовать класс от AppCompatActivity()
         }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val textview_info = findViewById<TextView>(R.id.textview_info)
+
+        if (requestCode == REQUEST_CHOOSE_THIEF) {
+            if (resultCode == Activity.RESULT_OK){
+                val thiefName = data?.getStringExtra(THIEF)
+                textview_info.text = thiefName
+            } else {
+                textview_info.text = ""
+            }
+        }
     }
 }
--- app/src/main/java/com/example/kotovod/SecondActivity.kt
+++ app/src/main/java/com/example/kotovod/SecondActivity.kt
@@ -1,11 +1,32 @@
 package com.example.kotovod
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.RadioGroup
 
 class SecondActivity: Activity() {
+
+    companion object{
+        const val THIEF = "com.example.kotovod.SecondActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+
+        radioGroup.setOnCheckedChangeListener { _, optionId ->
+            val answerIntent = Intent()
+            when(optionId){
+                R.id.radio_dog -> answerIntent.putExtra(THIEF, "Сраный песик")
+                R.id.radio_crow -> answerIntent.putExtra(THIEF, "Сраная ворона")
+                R.id.radio_cat -> answerIntent.putExtra(THIEF, "Лошадь Пржевальского")
+            }
+
+            setResult(RESULT_OK, answerIntent)
+            finish()
+        }
     }
 }

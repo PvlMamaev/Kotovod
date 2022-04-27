1af44f1bdd6df92bad9213b64b6c76bfcb0cb960
PvlMamaev <95972919+PvlMamaev@users.noreply.github.com>
2022-04-27 19:20:39 +0300

    mistake

--- app/src/main/java/com/example/kotovod/MainActivity.kt
+++ app/src/main/java/com/example/kotovod/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.kotovod
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_landscape)
+
+        fun getStringOrientation (): String {
+            return when (resources.configuration.orientation){
+                Configuration.ORIENTATION_LANDSCAPE -> "Альбомная ориентация"
+                Configuration.ORIENTATION_PORTRAIT -> "Портретная ориентация"
+                else -> ""
+            }
+        }
+
+        val button1 = findViewById<Button>(R.id.button1)
+        val editText = findViewById<EditText>(R.id.editText)
+
+        button1.setOnClickListener {
+            var str: String
+            str = getStringOrientation()
+            // editText.text = getStringOrientation() а чо нельзя???
+            editText.text = str
+        }
     }
+
+
+
+
 }

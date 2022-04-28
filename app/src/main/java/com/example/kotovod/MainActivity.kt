91fa761360899ac46c10c91cf6f78c5cdb5b7ae2
PvlMamaev <95972919+PvlMamaev@users.noreply.github.com>
2022-04-28 17:07:03 +0300

    add: onSavedInstanceState

--- app/src/main/java/com/example/kotovod/MainActivity.kt
+++ app/src/main/java/com/example/kotovod/MainActivity.kt
@@ -7,6 +7,10 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var counter: Int = 0
+    var counter_cats: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_linlay)
@@ -15,8 +19,6 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.button_Privetstvie)
         val button_counter_birds = findViewById<Button>(R.id.button_count_bird)
         val button_count_cats = findViewById<Button>(R.id.button_count_cats)
-        var counter: Int = 0
-        var counter_cats: Int = 0
 
         button.setOnClickListener {
             textView.text = "Hello Kitty!" // что он от меня хочет ?!
@@ -33,5 +35,23 @@ class MainActivity : AppCompatActivity() {
             textView.text = "Я насчитал ${++counter_cats} котиков"
             button_count_cats.setBackgroundColor(Color.GRAY)
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        val textView = findViewById<TextView>(R.id.textview_main)
+
+        outState.putString("KEY_TEXT", textView.text.toString())
+        outState.putInt("KEY_COUNT", counter)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        val textView = findViewById<TextView>(R.id.textview_main)
+
+        textView.text = savedInstanceState.getString("KEY_TEXT")
+        counter = savedInstanceState.getInt("KEY_COUNT")
     }
 }

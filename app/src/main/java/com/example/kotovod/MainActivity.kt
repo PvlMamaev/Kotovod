66c87e3111c3ff74271ac23fca70689aa3535dc5
PvlMamaev <95972919+PvlMamaev@users.noreply.github.com>
2022-04-29 14:25:38 +0300

    add: image

--- app/src/main/java/com/example/kotovod/MainActivity.kt
+++ app/src/main/java/com/example/kotovod/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.kotovod
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    val text = "Пора покормить кота!"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toast = Toast.makeText(applicationContext, text, Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        val button = findViewById<Button>(R.id.button)
+        val toastContainer = toast.view as LinearLayout
+        val catImage = ImageView(this)
+        catImage.setImageResource(R.drawable.cat)
+        toastContainer.addView(catImage, 0)
+        toastContainer.setBackgroundColor(Color.TRANSPARENT)
+
+        button.setOnClickListener {
+            toast.show()
+        }
     }
 }

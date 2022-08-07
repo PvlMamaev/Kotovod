0a1a3815b93135ffe12ec76cf33130d34509c4c6
PvlMamaev <95972919+PvlMamaev@users.noreply.github.com>
2022-08-07 16:37:09 +0300

    Update gradle

--- app/src/main/java/com/example/kotovod/MainActivity.kt
+++ app/src/main/java/com/example/kotovod/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.kotovod
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
